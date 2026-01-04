556f04b172dc1c491c24753700ec00190e753ac7
shine1i <wasimysdev@gmail.com>
2026-01-04 15:53:29 +0100

    test

--- src/gec/config.py
+++ src/gec/config.py
@@ -46,7 +46,7 @@ class GECConfig(BaseSettings):
     greco_weight: float = 0.6
     semantic_weight: float = 0.3
     laziness_weight: float = 0.1
-    gain_epsilon: float = 0.02
+    gain_epsilon: float = 0.01
 
     # Reward model paths
     greco_model_name: str = "mrqorib/grammaticality"
